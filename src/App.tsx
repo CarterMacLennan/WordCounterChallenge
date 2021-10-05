29d554ec92a16f52b760848ad8d652d605234997
CarterMacLennan <cartermaclennannn@gmail.com>
2021-10-05 15:10:01 -0300

    Initial Push

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
-import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import { Jumbotron, Button, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 function App() {
+  const [input, setInput] = useState("");
+  const [output, setOutput] = useState("");
+  const [filterByCount, setFilterByCount] = useState(true);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+
+  const toggleDropDown = () => setDropdownOpen(prevState => !prevState);
+
+  const countWords = () => {
+    let wordCounts: { [key: string]: number } = {}
+    input
+      .toLowerCase()
+      .split(" ")
+      .filter(str => str !== "")
+      .forEach( word => {
+      if (!wordCounts.hasOwnProperty(word)) {
+        wordCounts[word] =  1
+      }
+      else {
+        wordCounts[word]++
+      }
+    })
+
+    let result = ""
+    if (filterByCount) {
+      Object.keys(wordCounts)
+      .sort((a, b) => wordCounts[b] - wordCounts[a])
+      .map( key => result += key + ": " + wordCounts[key] + "\n")
+    }
+    else {
+      Object.keys(wordCounts)
+      .sort()
+      .map( key => result += key + ": " + wordCounts[key] + "\n")
+    }
+    setOutput(result)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <div>
+      <div className="app-header">
+        <Jumbotron >
+          <h1 className="title">Word Counter</h1>
+          <p className="subtitle">Just type or paste your text into the text area and we'll count them for you! </p>
+          <hr/>
+          <Button className="header-btn-custom">Contact the Developer</Button>
+        </Jumbotron>
+      </div>
+        <div className="row align-items-center">
+          <div className="col-sm-1"></div>
+          <div className="col-sm-4">
+            <div className="form-group ">
+              <textarea 
+                id="exampleTextarea" 
+                rows={10} 
+                className="input-field form-control form-control-lg mb-3word-input" 
+                placeholder="type here ..." 
+                value={input} 
+                onChange={(
+                  e: React.ChangeEvent<HTMLTextAreaElement>,
+                  ): void => setInput(e.target.value)
+                }
+              >  
+            </textarea>
+            </div> 
+          </div> 
+          <div className="col-sm-1 text-center">
+            <Dropdown size="lg" isOpen={dropdownOpen} toggle={toggleDropDown}>
+              <DropdownToggle className="dropdown-custom" caret>
+                Fillter By
+              </DropdownToggle>
+              <DropdownMenu>
+                <DropdownItem 
+                  onClick={() => setFilterByCount(false)}
+                  disabled = {!filterByCount}
+                >
+                  Alphabetically
+                </DropdownItem>
+                <DropdownItem 
+                  onClick={() => setFilterByCount(true)}
+                  disabled = {filterByCount}
+                >
+                  Numerically
+                </DropdownItem>
+              </DropdownMenu>
+            </Dropdown>
+          </div>
+          <div className="col-sm-1 text-center">
+            <Button 
+                className="btn-lg btn-custom center"
+                onClick={countWords}
+              >
+                Count!
+              </Button>
+          </div>
+          <div className="col-sm-4">
+            <div className="form-group ">
+              <textarea 
+              id="exampleTextarea" 
+              rows={10} 
+              className="input-field form-control form-control-lg mb-3word-input" 
+              value={output} 
+              />
+            </div> 
+          </div> 
+          <div className="col-sm-1"></div>
+        </div>
+    </div> 
+  )
 }
 
 export default App;
